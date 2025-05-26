3b44fef67cae387fe328d8d66157660ac898beeb
AgujinBot <flavioaibujesc.2005@gmail.com>
2025-05-25 20:42:10 -0500

    Update Habitacion.java

--- Habitacion.java
+++ Habitacion.java
@@ -1,1 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
 
+public class Habitacion   {
+
+    private int id;
+    private boolean reservada;
+
+    public Habitacion(int id, boolean reservada) {
+        this.id = id;
+        this.reservada = reservada;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public boolean isReservada() {
+        return reservada;
+    }
+
+    public void setReservada(boolean reservada) {
+        this.reservada = reservada;
+    }
+
+}
